5ea2b8aceb2b56e9affe1079c44c7b0028ec38f8
bojango <117446148+bojango@users.noreply.github.com>
2026-09-05 20:40:22 +0100

    Refresh cached Terminal FX assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='useful-tools-v6';
+const CACHE='useful-tools-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./tools-menu.js','./password/','./password/index.html','./palette/','./palette/index.html','./images/','./images/index.html','./images/app.js','./images/raw-worker.js','./images/coi-serviceworker.js','./terminalfx/','./terminalfx/index.html','./terminalfx/style.css','./terminalfx/app.js'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
